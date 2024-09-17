f9346a64c03e7ff139572665c84cfd1fdab0135e
miniscandal <oscar01dev@gmail.com>
2024-09-17 16:48:20 -0600

    refactor: add new asset, image png of the character anime

--- src/feature-start-screen/components/organisms/game-artwork/index.js
+++ src/feature-start-screen/components/organisms/game-artwork/index.js
@@ -1,16 +1,21 @@
 import { Picture } from '../../../../shared/components/atoms/picture';
-import { pictureKawaiiGamerDog } from '../../../../shared/components/atoms/picture/variants';
+import { pictureAnimeCharacter } from '../../../../shared/components/atoms/picture/variants';
+
+import { GridFrameFourByFour } from '../../molecules/grid-frame-four-by-four';
+import { gridFrameFourByFourArt } from '../../molecules/grid-frame-four-by-four/variants';
 
 import './style.css';
 
 function GameArtwork() {
-    const picture = Picture(pictureKawaiiGamerDog);
+    const picture = Picture(pictureAnimeCharacter);
 
+    const gridFrameFourByFour = GridFrameFourByFour(gridFrameFourByFourArt);
 
     return (
         `
         <section class="game-artwork">
             ${picture}
+            ${gridFrameFourByFour}
         </section>
         `
     );
