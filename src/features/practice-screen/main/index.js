c5a8d598d95b1a131c42c269ada2b176dbae3062
miniscandal <oscar01dev@gmail.com>
2024-09-28 19:47:21 -0600

    feat: implement new style for GridFrameFourByFour component

--- src/features/practice-screen/main/index.js
+++ src/features/practice-screen/main/index.js
@@ -1,6 +1,9 @@
 import { TitleH2 } from '@shared-components/atoms/title-h2';
 import { Screen } from '@shared-components/templates/screen';
 
+import { GridFrameFourByFour } from '@shared-components/molecules/grid-frame-four-by-four';
+import { gridFrameFourByFourPractice } from '@shared-components/molecules/grid-frame-four-by-four/variants';
+
 import './style.css';
 
 function PracticeScreen({ Game, screenId }) {
@@ -8,12 +11,21 @@ function PracticeScreen({ Game, screenId }) {
         text: 'Select Practice Puzzle'
     });
 
+    const elements = GridFrameFourByFour(gridFrameFourByFourPractice);
+
     const children = (
         `
         <article class="practice-screen">
             ${titleH2}
-            <div>game play</div>
-            <div>messages</div>
+            <section class="practice-screen__section">
+                ${elements}
+                ${elements}
+                ${elements}
+                ${elements}
+                ${elements}
+                ${elements}
+            </section>
+            <div>Game Tips...</div>
         </article>
         `
     );
---
+++ src/shared/components/molecules/grid-frame-four-by-four/variants.js
@@ -0,0 +1,19 @@
+const gridFrameFourByFourArt = {
+    playEnabled: false,
+    size: 'regular',
+    puzzleTile: {
+        size: 'auto',
+        playEnabled: false
+    }
+};
+
+const gridFrameFourByFourPractice = {
+    playEnabled: false,
+    size: 'small',
+    puzzleTile: {
+        size: 'auto',
+        playEnabled: false
+    }
+};
+
+export { gridFrameFourByFourArt, gridFrameFourByFourPractice };
