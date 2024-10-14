d4090a1a287aef8fc182ff3b636efd015edebf0f
miniscandal <oscar01dev@gmail.com>
2024-10-14 17:57:40 -0600

    feat: add stage sequence as constant

---
+++ src/core/constants/stage-sequence.js
@@ -0,0 +1,12 @@
+const STAGE_SEQUENCE = [
+    'puzzleA2',
+    'puzzleA3',
+    'puzzleA4',
+    'puzzleA5',
+    'puzzleA6',
+    'puzzleA7',
+    'puzzleA8',
+    'puzzleA9',
+];
+
+export { STAGE_SEQUENCE };
--- src/features/play-screen/components/organisms/puzzle-game/logic/load-puzzle/index.js
+++ src/features/play-screen/components/organisms/puzzle-game/logic/load-puzzle/index.js
@@ -1,19 +1,18 @@
 import { PuzzleGrid } from '@shared-components/organisms/puzzle-grid';
 
+import { STAGE_SEQUENCE } from '@core-constants/stage-sequence';
+
 import { PUZZLE_GAME_ID } from '../constants/element-identifiers';
 
 async function loadPuzzle({ Game, puzzleGridGamePlay }) {
     try {
-        const id = Game.puzzle.id;
-        const { ['puzzleA1']: puzzle } = await import('@core-constants/puzzles.js');
-
+        const id = Game.puzzle.id ?? STAGE_SEQUENCE[0];
+        const { [id]: puzzle } = await import('@core-constants/puzzles.js');
         const puzzleGrid = PuzzleGrid({
             ...puzzleGridGamePlay,
             puzzle
         });
-
         const puzzleGame = document.getElementById(PUZZLE_GAME_ID);
-
         const parser = new DOMParser();
         const node = parser.parseFromString(puzzleGrid, 'text/html');
         const newChild = node.body.firstElementChild;
@@ -21,7 +20,7 @@ async function loadPuzzle({ Game, puzzleGridGamePlay }) {
 
         puzzleGame.replaceChild(newChild, oldChild);
 
-        console.log(id);
+        Game.puzzle.id = id;
     } catch (error) {
         console.log(error);
     }
