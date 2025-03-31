122e10444b3ecacecbdddd2dbf08bbde8b99cbc8
miniscandal <oscar01dev@gmail.com>
2025-03-30 20:25:54 -0600

    feat: add new utility functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { ScreenSetupController } from '@core-controllers/screen-setup/main';
 import { ScreenModeController } from '@core-controllers/screen-mode/main';
 
-import { START_SCREEN } from '@shared-constants/screen-modes';
+import { GAME_SCREEN_START } from '@shared-constants/screen-modes';
 
 import './main.css';
 
 
-ScreenSetupController.routine(ScreenModeController.modes[START_SCREEN]());
+ScreenSetupController.routine(ScreenModeController.modes[GAME_SCREEN_START]());
---
+++ src/shared/utils/game-state-generate/index.js
@@ -0,0 +1,33 @@
+import { MAX_TILES } from '@shared-constants/puzzle-grid-settings';
+
+function createPermutation() {
+
+    return Array.from({ length: MAX_TILES + 1 }, (_, index) => index);
+}
+
+function shuffleSimplePuzzleState(permutation) {
+    const shuffled = [...permutation];
+    const a = shuffled[0];
+    shuffled[0] = shuffled[1];
+    shuffled[1] = a;
+
+    return shuffled;
+}
+
+function shufflePuzzleState(permutation) {
+    const shuffled = [...permutation];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    return shuffled;
+}
+
+
+export {
+    shuffleSimplePuzzleState,
+    shufflePuzzleState,
+    createPermutation
+};
