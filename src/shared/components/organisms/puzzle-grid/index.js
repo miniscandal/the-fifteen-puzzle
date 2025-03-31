27296e026067a63b6bf067ed008d77bc28cadee8
miniscandal <oscar01dev@gmail.com>
2025-03-30 20:20:40 -0600

    feat: convert puzzle permutation values to strings for consistency, modify value in the return object

--- src/shared/components/organisms/puzzle-grid/index.js
+++ src/shared/components/organisms/puzzle-grid/index.js
@@ -15,7 +15,7 @@ function PuzzleGrid({
     gameActive = false,
     puzzle = {
         id: undefined,
-        permutation: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        permutation: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
         state: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         description: '',
         movableTileIndices: [
@@ -24,31 +24,23 @@ function PuzzleGrid({
         ],
         enabled: true
     },
-    puzzleTile = {
-        id: undefined,
-        symbol: undefined,
-        size: 'auto',
-        playEnabled: false,
-        movementDirection: undefined,
-        row: undefined,
-        column: undefined,
-        index: undefined
-    }
+    puzzleTile = {}
 }) {
-    const { id, movableTileIndices, state } = puzzle;
+    const { id, permutation, state, movableTileIndices } = puzzle;
     const puzzleTiles = state.map((value, index) => {
         const movableTile = movableTileIndices.find(tile => tile.index === index);
 
+
         return PuzzleTile({
             id: value,
-            symbol: value,
+            symbol: permutation[value],
             size: 'auto',
             playEnabled: !!movableTile && playEnabled,
             movementDirection: movableTile ? movableTile.movementDirection : null,
             row: Math.floor(index / GRID_ROW) + 1,
             column: (index % GRID_COLUMN) + 1,
             index,
-            ...'puzzleTile' in arguments[0] ? puzzleTile : {},
+            ...(puzzleTile ? puzzleTile : {})
         });
     });
 
