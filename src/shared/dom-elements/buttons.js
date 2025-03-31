ea14f49ae122142b85f4b69168bbcabb3a31291f
miniscandal <oscar01dev@gmail.com>
2025-03-30 20:23:13 -0600

    refactor: update button element selectors with new constants

--- src/shared/dom-elements/buttons.js
+++ src/shared/dom-elements/buttons.js
@@ -1,16 +1,16 @@
 import { COLOR_THEME_ID } from '@shared-constants/dom-element-identifiers';
 
-import { SELECT_PRACTICE_SCREEN_ID } from '@shared-constants/dom-element-identifiers';
-import { SELECT_START_SCREEN_ID } from '@shared-constants/dom-element-identifiers';
-import { SELECT_PLAY_SCREEN_ID } from '@shared-constants/dom-element-identifiers';
+import { SELECT_GAME_SCREEN_PRACTICE_ID } from '@shared-constants/dom-element-identifiers';
+import { SELECT_GAME_SCREEN_START_ID } from '@shared-constants/dom-element-identifiers';
+import { SELECT_GAME_SCREEN_PLAY_ID } from '@shared-constants/dom-element-identifiers';
 import { SELECT_BACK_SCREEN_ID } from '@shared-constants/dom-element-identifiers';
 
 
 const domElementIconButtonColorTheme = () => document.getElementById(COLOR_THEME_ID);
 
-const domElementButtonSelectPracticeScreen = () => document.getElementById(SELECT_PRACTICE_SCREEN_ID);
-const domElementButtonSelectStartScreen = () => document.getElementById(SELECT_START_SCREEN_ID);
-const domElementButtonSelectPlayScreen = () => document.getElementById(SELECT_PLAY_SCREEN_ID);
+const domElementButtonSelectPracticeScreen = () => document.getElementById(SELECT_GAME_SCREEN_PRACTICE_ID);
+const domElementButtonSelectStartScreen = () => document.getElementById(SELECT_GAME_SCREEN_START_ID);
+const domElementButtonSelectPlayScreen = () => document.getElementById(SELECT_GAME_SCREEN_PLAY_ID);
 const domElementButtonSelectBackScreen = () => document.getElementById(SELECT_BACK_SCREEN_ID);
 
 export {
