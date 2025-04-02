4037f15ef16fc3b31e5281c554a8f397af19569e
miniscandal <oscar01dev@gmail.com>
2025-04-01 22:51:35 -0600

    fix: replace deprecated method, implement new method involved with UI rendering

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import { ScreenSetupController } from '@core-controllers/screen-setup/main';
 import { ScreenModeController } from '@core-controllers/screen-mode/main';
+import { PrefersColorSchemeController } from '@core-controllers/prefers-color-scheme/main';
+import { GameModeController } from '@core-controllers/game-mode/main';
+import { PuzzleGridController } from '@core-controllers/puzzle-grid/main';
 
 import { GAME_SCREEN_START } from '@shared-constants/screen-modes';
 
 import './main.css';
 
 
-ScreenSetupController.routine(ScreenModeController.modes[GAME_SCREEN_START]());
+ScreenSetupController.routine(ScreenModeController.transitionTo({
+    modeId: GAME_SCREEN_START,
+    coreControllers: {
+        ScreenSetupController,
+        PrefersColorSchemeController,
+        GameModeController,
+        ScreenModeController,
+        PuzzleGridController
+    }
+}));
