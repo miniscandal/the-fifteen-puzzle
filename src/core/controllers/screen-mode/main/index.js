bbb8248ca6b5b2af0a782ac770d5dd8d924e2b76
miniscandal <oscar01dev@gmail.com>
2025-04-01 22:28:14 -0600

    refactor: implement ScreenModeController as a factory function

--- src/core/controllers/screen-mode/main/index.js
+++ src/core/controllers/screen-mode/main/index.js
@@ -1,8 +1,3 @@
-import { ScreenSetupController } from '@core-controllers/screen-setup/main';
-import { PrefersColorSchemeController } from '@core-controllers/prefers-color-scheme/main';
-import { GameModeController } from '@core-controllers/game-mode/main';
-import { PuzzleGridController } from '@core-controllers/puzzle-grid/main';
-
 import { startMode } from '@core-controllers/screen-mode/start-mode/main';
 import { practiceMode } from '@core-controllers/screen-mode/practice-mode/main';
 import { playMode } from '@core-controllers/screen-mode/play-mode/main';
@@ -12,42 +7,52 @@ import { GAME_SCREEN_PRACTICE } from '@shared-constants/screen-modes';
 import { GAME_SCREEN_START } from '@shared-constants/screen-modes';
 
 
-const ScreenModeController = {
-    previousMode: undefined,
-    currentMode: undefined,
-    modeHistory: [],
-    modes: {
-        [GAME_SCREEN_START]: () => ScreenModeController.updateMode(GAME_SCREEN_START, startMode),
-        [GAME_SCREEN_PRACTICE]: () => ScreenModeController.updateMode(GAME_SCREEN_PRACTICE, practiceMode),
-        [GAME_SCREEN_PLAY]: () => ScreenModeController.updateMode(GAME_SCREEN_PLAY, playMode),
-    },
-    updateMode(newMode, modeFunction) {
-        this.previousMode = this.currentMode;
-        this.currentMode = newMode;
-        this.modeHistory.push(newMode);
+function createScreenModeManager() {
+    let previousMode = undefined;
+    let currentMode = undefined;
+    let modeHistory = [];
 
-        return modeFunction(coreObjects);
-    },
-    lastModeHistory() {
-        /**
-         * Delete the current mode screen in history
-         */
-        this.modeHistory.pop();
+    const modesConfig = {
+        [GAME_SCREEN_START]: { handlerMode: startMode },
+        [GAME_SCREEN_PRACTICE]: { handlerMode: practiceMode },
+        [GAME_SCREEN_PLAY]: { handlerMode: playMode },
+    };
 
-        return this.modeHistory.pop();
-    },
-    clearModeHistory() {
-        this.modeHistory = this.modeHistory.slice(-1);
-    }
+    const updateMode = (modeId) => {
+        previousMode = currentMode;
+        currentMode = modeId;
+        modeHistory.push(modeId);
+    };
+
+    const transitionTo = ({ modeId, coreControllers }) => {
+        const { handlerMode } = modesConfig[modeId];
+
+        updateMode(modeId);
+
+        console.log(coreControllers);
+
+        return handlerMode(coreControllers);
+    };
+
+    const lastModeHistory = () => {
+        modeHistory.pop();
+
+        return modeHistory.pop();
+    };
+
+    const clearModeHistory = () => modeHistory = modeHistory.slice(-1);
+
+
+    return {
+        get previousMode() { return previousMode; },
+        get currentMode() { return currentMode; },
+        get modeHistory() { return modeHistory; },
+        get lastModeHistory() { return lastModeHistory; },
+        transitionTo,
+        clearModeHistory
+    };
 };
 
-const coreObjects = {
-    ScreenSetupController,
-    PrefersColorSchemeController,
-    GameModeController,
-    ScreenModeController,
-    PuzzleGridController
-};
-
+const ScreenModeController = createScreenModeManager();
 
 export { ScreenModeController };
