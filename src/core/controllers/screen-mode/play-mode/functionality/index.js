0d0864ddd4e45dd3189b7afc51827726a6faa5a6
miniscandal <oscar01dev@gmail.com>
2025-04-02 18:14:49 -0600

    refactor: rename shiffle function, extract core controllers and optimize state

--- src/core/controllers/screen-mode/play-mode/functionality/index.js
+++ src/core/controllers/screen-mode/play-mode/functionality/index.js
@@ -4,11 +4,13 @@ import { selectPuzzleTile } from '@feat-puzzle-grid/select-puzzle-tile';
 import { configureColorSchemePreference } from '@feat-prefers-color-scheme/configure-color-scheme-preference';
 import { addEventListenerSelectScreenMode } from '@feat-screen-mode/add-event-listener-select-screen-mode';
 
-import { domElementButtonSelectBackScreen } from '@shared-dom-elements/buttons';
+import { domElementButtonSelectBackScreen, domElementButtonSelectStartScreen } from '@shared-dom-elements/buttons';
 
 import { getAdjacentIndicesInGrid } from '@shared-utils/get-adjacent-indices-in-grid';
 import { getItemPositionInGrid } from '@shared-utils/get-item-position-in-grid';
-import { createPermutation, shuffleSimplePuzzleState } from '@shared-utils/game-state-generate';
+import { createPermutation, shufflePuzzleState } from '@shared-utils/game-state-generate';
+
+import { GAME_SCREEN_START } from '@shared-constants/screen-modes';
 
 
 async function playModeFunctionality({
@@ -21,19 +23,31 @@ async function playModeFunctionality({
     const { currentMode, modes } = GameModeController;
     const { puzzleId } = modes[currentMode]({ PuzzleGridController });
     const { puzzle } = await loadPuzzle({ puzzleId });
-    const state = shuffleSimplePuzzleState(createPermutation());
+    const state = shufflePuzzleState(createPermutation());
+    const coreGameControllers = {
+        ScreenSetupController,
+        PrefersColorSchemeController,
+        GameModeController,
+        ScreenModeController,
+        PuzzleGridController
+    };
     const onSelectPuzzleTile = (index) => {
         const state = PuzzleGridController.updateState(index);
         const isSolved = PuzzleGridController.checkState(state);
 
-        //TODO: implement modal component
-        console.log(isSolved);
+        if (isSolved) {
+            // code
+        }
     };
+
+    const movableTileIndices = getAdjacentIndicesInGrid(getItemPositionInGrid(state.map(mappedPos => (
+        puzzle.permutation[mappedPos]
+    ))));
 
     PuzzleGridController.puzzle = {
         ...puzzle,
-        state: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        movableTileIndices: getAdjacentIndicesInGrid(getItemPositionInGrid([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
+        state,
+        movableTileIndices
     };
 
     renderPuzzleScene({ puzzle: PuzzleGridController.puzzle });
@@ -45,13 +59,17 @@ async function playModeFunctionality({
         updateScreenMode: () => {
             ScreenSetupController.routine(ScreenModeController.transitionTo({
                 modeId: ScreenModeController.lastModeHistory(),
-                coreControllers: {
-                    ScreenSetupController,
-                    PrefersColorSchemeController,
-                    GameModeController,
-                    ScreenModeController,
-                    PuzzleGridController
-                }
+                coreGameControllers
+            }));
+        }
+    });
+
+    addEventListenerSelectScreenMode({
+        getElement: domElementButtonSelectStartScreen,
+        updateScreenMode: () => {
+            ScreenSetupController.routine(ScreenModeController.transitionTo({
+                modeId: GAME_SCREEN_START,
+                coreGameControllers
             }));
         }
     });
