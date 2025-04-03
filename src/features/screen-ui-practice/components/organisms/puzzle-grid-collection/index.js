0ff4a2f54df3f25bbaa421731c524ec5df50cb5b
miniscandal <oscar01dev@gmail.com>
2025-04-02 18:44:32 -0600

    feat: implement the correct state property

--- src/features/screen-ui-practice/components/organisms/puzzle-grid-collection/index.js
+++ src/features/screen-ui-practice/components/organisms/puzzle-grid-collection/index.js
@@ -15,7 +15,7 @@ function PuzzleGridCollection({ puzzles = [] }) {
             ...puzzleGridPractice,
             puzzle: {
                 ...puzzle,
-                state: puzzle.permutation,
+                state: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
                 movableTileIndices: []
             }
         });
