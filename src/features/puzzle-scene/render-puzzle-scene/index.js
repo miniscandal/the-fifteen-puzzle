841a6c6263e1f4a117c0b56ae95acc9cb60990a3
miniscandal <oscar01dev@gmail.com>
2025-04-04 19:02:01 -0600

    feat: render helper puzzle grid and extract DOM update logic

--- src/features/puzzle-scene/render-puzzle-scene/index.js
+++ src/features/puzzle-scene/render-puzzle-scene/index.js
@@ -1,22 +1,44 @@
 import { PuzzleGrid } from '@shared-components/organisms/puzzle-grid';
 
-import { puzzleGridGamePlay } from '@shared-components/organisms/puzzle-grid/variants';
+import {
+    PUZZLE_GAME_ID,
+    PUZZLE_GRID,
+    PUZZLE_HELPER_GAME,
+} from '@shared-constants/dom-element-identifiers';
 
-import { PUZZLE_GAME_ID } from '@shared-constants/dom-element-identifiers';
+import { createPermutation } from '@shared-utils/game-state-generate';
 
+
+function updateDOMElementWithGrid(containerId, gridHTML) {
+    const container = document.getElementById(containerId);
+
+    const parser = new DOMParser();
+    const parsedDoc = parser.parseFromString(gridHTML, 'text/html');
+    const newChild = parsedDoc.body.firstElementChild;
+    const oldChild = container.firstElementChild;
+
+    container.replaceChild(newChild, oldChild);
+}
 
 function renderPuzzleScene({ puzzle }) {
-    const puzzleGrid = PuzzleGrid({
-        ...puzzleGridGamePlay,
-        puzzle
+    const mainGridHTML = PuzzleGrid({
+        id: PUZZLE_GRID,
+        size: 'medium',
+        gameActive: true,
+        puzzle,
     });
-    const puzzleGame = document.getElementById(PUZZLE_GAME_ID);
-    const parser = new DOMParser();
-    const node = parser.parseFromString(puzzleGrid, 'text/html');
-    const newChild = node.body.firstElementChild;
-    const oldChild = puzzleGame.firstElementChild;
 
-    puzzleGame.replaceChild(newChild, oldChild);
+    updateDOMElementWithGrid(PUZZLE_GAME_ID, mainGridHTML);
+
+    const helperGridHTML = PuzzleGrid({
+        size: 'small',
+        puzzle: {
+            ...puzzle,
+            state: createPermutation(),
+        },
+    });
+
+    updateDOMElementWithGrid(PUZZLE_HELPER_GAME, helperGridHTML);
 }
 
 export { renderPuzzleScene };
