1b6f1531fa7db897d7c2290e1b382bd37329b378
miniscandal <oscar01dev@gmail.com>
2025-04-08 17:21:27 -0600

    fix: update selector to target active element

--- src/shared/constants/dom-element-data-attributes.js
+++ src/shared/constants/dom-element-data-attributes.js
@@ -1,7 +1,7 @@
 const DATA_ATTR_PLAY_ENABLED = 'data-play-enabled';
 const DATA_ATTR_MOVE_DIRECTION = 'data-movement-direction';
 const DATA_ATTR_PLAY_ENABLED_TILE = '[data-play-enabled="true"][data-symbol]';
-const DATA_ATTR_EMPTY_TILE = '[data-id="0"]';
+const DATA_ATTR_EMPTY_TILE = '.active [data-id="0"]';
 const DATA_ATTR_SYMBOL_TILE = 'data-symbol';
 
 const DATA_ATTR_INDEX_TILE = (index) => `[data-index="${index}"]`;
