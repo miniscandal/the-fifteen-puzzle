04e8557f339eec2404b343c24ac2913a905541be
miniscandal <oscar01dev@gmail.com>
2025-04-10 15:32:21 -0600

    refactor: remove unused props, initialize props based on the component state if of type game active and implement more descriptive css classes

--- src/shared/components/organisms/puzzle-grid/index.js
+++ src/shared/components/organisms/puzzle-grid/index.js
@@ -5,17 +5,17 @@ import { GRID_COLUMN } from '@shared-constants/puzzle-grid-settings';
 
 import { TILE_MOVE_LEFT } from '@shared-constants/movement-direction';
 import { TILE_MOVE_UP } from '@shared-constants/movement-direction';
+import { PUZZLE_GRID } from '@shared-constants/dom-element-identifiers';
 
 import './style.css';
 
 
 function PuzzleGrid({
-    id = null,
     size = 'regular',
     gameActive = false,
     pendingSelection = false,
     puzzle = {
-        id: null,
+        id: 'puzzleA0',
         permutation: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
         state: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         description: 'Sequential pattern of a solved Fifteen Puzzle',
@@ -33,25 +33,19 @@ function PuzzleGrid({
     },
     puzzleTile = {}
 }) {
-    const { permutation, state, movableTileIndices } = puzzle;
+    const { id, permutation, state, movableTileIndices } = puzzle;
 
     const puzzleTiles = state.map((value, index) => {
+
         const movableTile = movableTileIndices.find(tile => tile.index === index);
 
-        let id = '1';
-
-        if (gameActive) {
-            id = permutation[value];
-        } else {
-            id = permutation[value] === '0' ? 0 : '1';
-        }
 
         return PuzzleTile({
-            id,
+            id: gameActive ? permutation[value] : null,
             symbol: permutation[value],
             size: 'auto',
             playEnabled: !!movableTile && gameActive,
-            movementDirection: movableTile ? movableTile.movementDirection : null,
+            movementDirection: gameActive && movableTile ? movableTile.movementDirection : null,
             row: Math.floor(index / GRID_ROW) + 1,
             column: (index % GRID_COLUMN) + 1,
             index: gameActive ? index : null,
@@ -62,13 +56,13 @@ function PuzzleGrid({
     const classList = [
         gameActive ? 'active' : '',
         pendingSelection ? 'pending-selection' : '',
-        size
+        `size-${size}`
     ];
 
 
     return (
         `
-        <section id=${id} class="puzzle-grid ${classList.join(' ')}">
+        <section id=${PUZZLE_GRID} data-puzzle-id=${id} class="puzzle-grid ${classList.join(' ')}">
             ${puzzleTiles.join('')}
         </section>
         `
