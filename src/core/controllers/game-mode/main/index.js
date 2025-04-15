131aaa9d18d8657d191ea5c2163cc61d3d3a4346
miniscandal <oscar01dev@gmail.com>
2025-04-14 19:15:53 -0600

    refactor: remove unused imports, properties and constants

--- src/core/controllers/game-mode/main/index.js
+++ src/core/controllers/game-mode/main/index.js
@@ -1,17 +1,5 @@
-import { sequenceMode } from '@core-controllers/game-mode/sequence-mode/main';
-import { practiceMode } from '@core-controllers/game-mode/practice-mode/main';
-
-import { GAME_MODE_SEQUENCE } from '@shared-constants/game-modes';
-import { GAME_MODE_PRACTICE } from '@shared-constants/game-modes';
-
-
 const GameModeController = {
-    modes: {
-        [GAME_MODE_SEQUENCE]: ({ PuzzleGridController }) => sequenceMode({ PuzzleGridController }),
-        [GAME_MODE_PRACTICE]: ({ PuzzleGridController }) => practiceMode({ PuzzleGridController })
-    },
-    currentMode: null,
-    previousMode: null,
+    mode: null,
 };
 
 export { GameModeController };
