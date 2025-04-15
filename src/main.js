82befd846a60494b26dc311d8d60990ad16f03f0
miniscandal <oscar01dev@gmail.com>
2025-04-14 19:35:25 -0600

    fix: rename constant and property

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
-import { ScreenSetupController } from '@core-controllers/screen-setup/main';
-import { ScreenModeController } from '@core-controllers/screen-mode/main';
-import { PrefersColorSchemeController } from '@core-controllers/prefers-color-scheme/main';
 import { GameModeController } from '@core-controllers/game-mode/main';
 import { PuzzleGridController } from '@core-controllers/puzzle-grid/main';
+import { ScreenSetupController } from '@core-controllers/screen-setup/main';
+import { ScreenController } from '@core-controllers/screen/main';
+import { PuzzleSequenceController } from '@core-controllers/puzzle-sequence/main';
+import { PrefersColorSchemeController } from '@core-controllers/prefers-color-scheme/main';
 
 import { GAME_SCREEN_START } from '@shared-constants/screen-modes';
 
 import './main.css';
 
 
-ScreenSetupController.routine(ScreenModeController.transitionTo({
-    modeId: GAME_SCREEN_START,
-    coreGameControllers: {
-        ScreenSetupController,
-        PrefersColorSchemeController,
+ScreenSetupController.setup(ScreenController.transitionTo({
+    screenId: GAME_SCREEN_START,
+    GameCoreControllers: {
         GameModeController,
-        ScreenModeController,
-        PuzzleGridController
+        PuzzleGridController,
+        ScreenSetupController,
+        ScreenController,
+        PuzzleSequenceController,
+        PrefersColorSchemeController
     }
 }));
