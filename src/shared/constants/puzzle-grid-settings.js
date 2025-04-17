4633bdf0dd5483d7fcaeb67a025cf012b3768800
miniscandal <oscar01dev@gmail.com>
2025-04-16 18:24:26 -0600

    feat: add new constant TILES_PER_COLUMM

--- src/shared/constants/puzzle-grid-settings.js
+++ src/shared/constants/puzzle-grid-settings.js
@@ -1,4 +1,5 @@
 const TILES_PER_ROW = 4;
+const TILES_PER_COLUMN = 4;
 const EMPTY_TILE_VALUE = '0';
 const MOVEMENT_DISTANCE = 1;
 const FIRST_TILE_INDEX = 0;
@@ -8,6 +9,7 @@ const MAX_TILES = 15;
 
 export {
     TILES_PER_ROW,
+    TILES_PER_COLUMN,
     EMPTY_TILE_VALUE,
     MOVEMENT_DISTANCE,
     FIRST_TILE_INDEX,
