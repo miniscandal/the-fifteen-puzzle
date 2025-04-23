b80a51997403ecf03f927b969deea276cc2d573a
miniscandal <oscar01dev@gmail.com>
2025-04-23 15:46:57 -0600

    feat: add method to update solution status

--- src/core/controllers/puzzle-grid/main/index.js
+++ src/core/controllers/puzzle-grid/main/index.js
@@ -121,6 +121,36 @@ const PuzzleGridController = {
         return movableTileIndices;
     },
 
+    onPuzzleTileClick({ selectedTile, emptyTile, puzzle }) {
+        const emptyTileIndex = Number(emptyTile.dataset.index);
+
+        const solution = this.updateState({
+            solution: puzzle.solution,
+            selectedIndex: selectedTile.dataset.index,
+            zeroIndex: emptyTileIndex
+        });
+
+        puzzle.solution = solution;
+
+        const movableTileIndices = this.getMovableTileIndices({
+            solution,
+            permutation: puzzle.permutation
+        });
+
+        console.log(puzzle);
+
+        const { isSolved } = puzzle;
+
+        if (isSolved) {
+            console.log(isSolved);
+        }
+
+
+        return {
+            movableTileIndices
+        };
+    },
+
     async loadPuzzle({ id }) {
         try {
             const { [id]: puzzle } = await import('@shared-constants/puzzle-collection');
