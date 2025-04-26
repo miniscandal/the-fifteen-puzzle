086b40db9f5b4ab479b7ca2a59d4aa5c7151cc83
miniscandal <oscar01dev@gmail.com>
2025-04-26 17:36:12 -0600

    style: add new aliases

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
 
             '@core-controllers': '/src/core/controllers',
             '@core-factories': '/src/core/factories',
+            '@core-states': '/src/core/states',
 
             '@feat-game-mode': '/src/features/game-mode',
             '@feat-prefers-color-scheme': '/src/features/prefers-color-scheme',
             '@feat-puzzle-grid': '/src/features/puzzle-grid',
-            '@feat-puzzle-scene': '/src/features/puzzle-scene',
-            '@feat-screen-mode': '/src/features/screen-mode',
+            '@feat-screen': '/src/features/screen',
 
             '@feat-screen-ui-play': '/src/features/screen-ui-play',
             '@feat-screen-ui-practice': '/src/features/screen-ui-practice',
