f4e9c8a95b9bdb6ed8dcf7cb8ba01ea0adfb133a
miniscandal <oscar01dev@gmail.com>
2025-05-06 19:12:19 -0600

    feat: update screen state and properly pass global objects

--- src/core/controllers/screen/practice-handler/main/index.js
+++ src/core/controllers/screen/practice-handler/main/index.js
@@ -1,9 +1,33 @@
 import { Practice } from '@feat-screen-ui-practice/components/pages/practice';
 
 import { uiPracticeFunctionality } from '../functionality';
+import { SCREEN_ID_PRACTICE } from '@shared-constants/screen-modes';
 
 
 function practiceHandler({ coreControllers, coreFactories, coreState, domActions }) {
+    const setupGamePlay = ({ puzzleId }) => {
+
+        return {
+            puzzleId,
+            handlePuzzleSolved: () => { }
+        };
+    };
+
+
+
+    const { ScreenState } = coreState;
+
+    const newScreenState = coreControllers.ScreenController.updateScreenState({
+        state: ScreenState,
+        history: ScreenState.history,
+        screenId: SCREEN_ID_PRACTICE
+    });
+
+    coreState.ScreenState = newScreenState;
+
+    console.log(coreState.ScreenState);
+
+
 
     return {
         prepareHtmlStructure: () => Practice(coreState),
@@ -11,7 +35,8 @@ function practiceHandler({ coreControllers, coreFactories, coreState, domActions
             coreControllers,
             coreFactories,
             coreState,
-            domActions
+            domActions,
+            setupGamePlay
         })
     };
 };
