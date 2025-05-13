75dc2967cb69e4720c025d756b1a9ace10cf0ffe
miniscandal <oscar01dev@gmail.com>
2025-05-12 21:26:49 -0600

    feat: add new methods with dependency injection

--- src/main.js
+++ src/main.js
@@ -3,23 +3,21 @@ import { PuzzleGridShuffling } from '@core-controllers/puzzle-grid-shuffling-con
 import { PuzzleGridTiles } from '@core-controllers/puzzle-grid-tiles-controller';
 
 import { ScreenNavigatorController } from '@core-controllers/screen-navigator-controller';
-
 import { GameModeSequentialPuzzlesController } from '@core-controllers/sequential-puzzles-game-mode-controller';
 import { StartScreenController } from '@core-controllers/start-screen-controller';
 import { PracticeScreenController } from '@core-controllers/practice-screen-controller';
-
-import { PrefersColorSchemeState } from '@core-states/prefers-color-scheme-state';
+import { PuzzleGridDomController } from '@core-controllers/puzzle-grid-dom-controller';
+import { ScreenSetupDomController } from '@core-controllers/screen-setup-dom-controller';
+import { ScreenManagementDomController } from '@core-controllers/screen-management-dom-controller';
 
 import { PuzzleGridFactory } from '@core-factories/puzzle-grid-factory';
 
 import { ScreenState } from '@core-states/screen-state';
 import { GameModeState } from '@core-states/game-mode-state';
 import { GameModeSequentialPuzzlesState } from '@core-states/game-mode-sequential-puzzles-state';
-import { createPuzzleState } from '@core-states/puzzle-state';
+import { PrefersColorSchemeState } from '@core-states/prefers-color-scheme-state';
 
-import { PuzzleGridDomController } from '@core-controllers/puzzle-grid-dom-controller';
-import { ScreenSetupDomController } from '@core-controllers/screen-setup-dom-controller';
-import { ScreenManagementDomController } from '@core-controllers/screen-management-dom-controller';
+import { createPuzzleState } from '@core-states/puzzle-state';
 
 import { SCREEN_ID_START } from '@shared-constants/screen-modes';
 
