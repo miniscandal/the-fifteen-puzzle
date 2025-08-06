2d342900a5fa9617c3c709c931400a9cc85e8529
miniscandal <oscar01dev@gmail.com>
2025-08-05 23:02:02 -0600

    refactor: modify puzzles

--- src/shared/constants/puzzle-collection.js
+++ src/shared/constants/puzzle-collection.js
@@ -1,64 +1,85 @@
 const puzzleA1 = {
     id: 'puzzleA1',
-    permutation: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
-    description: 'Solved solution (ascending left to right, top to bottom)',
-    playable: false
+    permutation: ['4', '3', '2', '1', '5', '14', '13', '12', '6', '15', '0', '11', '7', '8', '9', '10'],
+    description: 'Peripheral',
+    playable: true
 };
 
 const puzzleA2 = {
     id: 'puzzleA2',
-    permutation: ['0', '12', '8', '4', '15', '11', '7', '3', '14', '10', '6', '2', '13', '9', '5', '1'],
-    description: 'Vertical reverse order from bottom to top',
+    permutation: ['10', '11', '12', '13', '9', '2', '3', '14', '8', '1', '4', '15', '7', '6', '5', '0'],
+    description: 'Spiral',
     playable: true
 };
 
 const puzzleA3 = {
     id: 'puzzleA3',
-    permutation: ['4', '3', '2', '1', '8', '7', '6', '5', '12', '11', '10', '9', '0', '15', '14', '13'],
-    description: 'Descending rows right-aligned (top to bottom)',
+    permutation: ['1', '2', '3', '4', '8', '7', '6', '5', '9', '10', '11', '12', '0', '15', '14', '13'],
+    description: 'Snake-Swirl',
     playable: true
 };
 
 const puzzleA4 = {
     id: 'puzzleA4',
-    permutation: ['1', '2', '3', '4', '12', '13', '14', '5', '11', '0', '15', '6', '10', '9', '8', '7'],
-    description: 'Snake-like horizontal ordering from bottom to top',
+    permutation: ['13', '14', '15', '0', '12', '11', '10', '9', '5', '6', '7', '8', '4', '3', '2', '1'],
+    description: 'Reverse-Snake Swirl',
     playable: true
 };
 
 const puzzleA5 = {
     id: 'puzzleA5',
-    permutation: ['15', '14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'],
-    description: 'Reversed (descending spiral from outside to center)',
+    permutation: ['1', '8', '9', '0', '2', '7', '10', '15', '3', '6', '11', '14', '4', '5', '12', '13'],
+    description: 'Down and Up',
     playable: true
 };
 
 const puzzleA6 = {
     id: 'puzzleA6',
-    permutation: ['5', '1', '13', '9', '6', '2', '14', '10', '7', '3', '15', '11', '4', '0', '12', '8'],
-    description: 'Continuous sequence wrapping vertically (bottom to top)',
+    permutation: ['4', '8', '12', '0', '3', '7', '11', '15', '2', '6', '10', '14', '1', '5', '9', '13'],
+    description: 'Vertical (Reverse)',
     playable: true
 };
 
 const puzzleA7 = {
     id: 'puzzleA7',
-    permutation: ['9', '1', '10', '2', '11', '3', '12', '4', '13', '5', '14', '6', '15', '7', '0', '8'],
-    description: 'Continuous vertical sequence (top to bottom, reversed)',
-    playable: true
+    permutation: ['1', '5', '9', '13', '2', '6', '10', '14', '3', '7', '11', '15', '4', '8', '12', '0'],
+    description: 'Vertical',
+    playable: false
 };
 
 const puzzleA8 = {
     id: 'puzzleA8',
-    permutation: ['3', '10', '7', '13', '4', '0', '1', '2', '5', '11', '8', '14', '6', '12', '9', '15'],
-    description: 'Diagonal pattern starting from lower-left corner',
-    playable: true
+    permutation: ['1', '3', '5', '7', '9', '11', '13', '15', '2', '4', '6', '8', '10', '12', '14', '0'],
+    description: 'Odds First',
+    playable: false
 };
 
 const puzzleA9 = {
     id: 'puzzleA9',
-    permutation: ['14', '11', '6', '3', '8', '15', '12', '7', '4', '9', '0', '13', '2', '5', '10', '1'],
-    description: 'Diagonal pattern starting from upper-right corner',
-    playable: true
+    permutation: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '0'],
+    description: 'Horizontal',
+    playable: false
+};
+
+const puzzleA10 = {
+    id: 'puzzleA10',
+    permutation: ['0', '15', '14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'],
+    description: 'Horizontal-Reverse',
+    playable: false
+};
+
+const puzzleA11 = {
+    id: 'puzzleA11',
+    permutation: ['4', '5', '12', '13', '3', '6', '11', '14', '2', '7', '10', '15', '1', '8', '9', '0'],
+    description: 'Up and Down',
+    playable: false
+};
+
+const puzzleA12 = {
+    id: 'puzzleA12',
+    permutation: ['2', '4', '6', '8', '10', '12', '14', '0', '1', '3', '5', '7', '9', '11', '13', '15'],
+    description: 'Even First',
+    playable: false
 };
 
 
@@ -68,8 +89,5 @@ export {
     puzzleA3,
     puzzleA4,
     puzzleA5,
-    puzzleA6,
-    puzzleA7,
-    puzzleA8,
-    puzzleA9
+    puzzleA6
 };
